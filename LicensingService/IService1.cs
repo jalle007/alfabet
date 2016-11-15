88595ad799a2f884534d784b849f341d937da152
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2016-11-15 01:36:23 +0100

    1st

---
+++ LicensingService/IService1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LicensingService {
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+
+    /*
+     * product id
+     * users
+     * features
+     * databases
+     * email
+     * generate rnd num on each number
+     */
+  [ServiceContract]
+  public interface IService1 {
+
+    [OperationContract]
+    string GetData(int value);
+
+    [OperationContract]
+    CompositeType GetDataUsingDataContract(CompositeType composite);
+
+    [OperationContract]
+    string GetNewSerial( );
+
+    [OperationContract]
+    bool GenerateSerials( int number);
+
+    [OperationContract]
+    bool isValidSerial(string serial);
+
+    [OperationContract]
+    bool AddSubscription(SubscribeViewModel model);
+
+    // TODO: Add your service operations here
+  }
+
+  [DataContract]
+  public class SubscribeViewModel {
+    [DataMember]
+    public string Email { get; set; }
+
+    [DataMember]
+    public int Users { get; set; }
+
+    [DataMember]
+    public int Features { get; set; }
+
+    [DataMember]
+    public int Databases { get; set; }
+
+    [DataMember]
+    public string Serial { get; set; }
+  }
+
+  // Use a data contract as illustrated in the sample below to add composite types to service operations.
+  [DataContract]
+  public class CompositeType {
+    bool boolValue = true;
+    string stringValue = "Hello ";
+
+    [DataMember]
+    public bool BoolValue {
+      get { return boolValue; }
+      set { boolValue = value; }
+    }
+
+    [DataMember]
+    public string StringValue {
+      get { return stringValue; }
+      set { stringValue = value; }
+    }
+  }
+}
---
+++ LicensingService/Service1.svc.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LicensingService {
+  public class Service1 : IService1 {
+    public dbEntities db = new dbEntities();
+
+    public bool AddSubscription(SubscribeViewModel model)
+    {
+      try {
+        Subscriptions sub = new Subscriptions() {
+          UserID = model.Email,
+          Users = model.Users,
+          Databases = model.Databases,
+          Features = model.Features,
+          Serial = model.Serial,
+          Active = true,
+          CreatedAt = DateTime.Now
+        };
+        db.Subscriptions.Add(sub);
+        db.SaveChanges();
+      } catch (Exception) {
+        return false;
+      }
+      return true;
+    }
+
+    //generate new serial
+    public string GetNewSerial() {
+      SerialsBank ser = (from d in db.SerialsBank
+                         where d.Active == true
+                         select d).FirstOrDefault();
+
+      if (ser == null)
+        return null;
+      //else
+      ser.Active = false;
+      db.SaveChanges();
+      return ser.Serial;
+    }
+
+    //check if serial is valid
+    public bool isValidSerial(string serial) {
+      SerialsBank ser = (from d in db.SerialsBank
+                         where d.Serial == serial
+                         select d).FirstOrDefault();
+
+      return ser!=null;
+    }
+
+    // generate new n serial keys and store it in database
+    public bool GenerateSerials(int number) {
+      List<SerialsBank> list1 = new List<SerialsBank>();
+      try {
+        for (int i = 0; i < number; i++) {
+          Guid guid = Guid.NewGuid();
+          list1.Add(new SerialsBank() { Serial = guid.ToString(), Active = true, CreatedAt = DateTime.Now });
+        }
+
+        db.SerialsBank.AddRange(list1);
+        db.SaveChanges();
+      } catch (Exception) {
+        return false;
+        //  throw;
+      }
+      return true;
+    }
+
+
+    public string GetData(int value) {
+      return string.Format("You entered: {0}", value);
+    }
+
+    public CompositeType GetDataUsingDataContract(CompositeType composite) {
+      if (composite == null) {
+        throw new ArgumentNullException("composite");
+      }
+      if (composite.BoolValue) {
+        composite.StringValue += "Suffix";
+      }
+      return composite;
+    }
+  }
+}
